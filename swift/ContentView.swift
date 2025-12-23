cc5766cbe434564d64d3eccd8be922a4cee222c9
CHEN,CHANG-JIE <D1397009@o365.fcu.edu.tw>
2025-12-23 09:48:30 +0800

    Initial Commit

---
+++ swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift
+//
+//  Created by 訪客使用者 on 2025/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftApp.swift
+//  swift
+//
+//  Created by 訪客使用者 on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
